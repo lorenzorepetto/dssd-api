80bb7aefc6c83aa8b5aa1200072ee2f868494c19
Lorenzo Repetto <lorenzo.repetto@mercadolibre.com>
2021-10-02 17:10:48 -0300

    elimino objeto customer de ejemplo

--- src/main/java/com/dssd/grupo15/backend/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dssd.grupo15.backend.repository;
-
-import com.dssd.grupo15.backend.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
